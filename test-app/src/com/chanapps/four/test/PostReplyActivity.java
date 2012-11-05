8e5f08576f5d6559ea8e3d96365ac1055e986597
John Arley Burns <johnarleyburns@gmail.com>
2012-11-05 19:58:27 +0100

    now with image attachment

--- test-app/src/com/chanapps/four/test/PostReplyActivity.java
+++ test-app/src/com/chanapps/four/test/PostReplyActivity.java
@@ -1,39 +1,31 @@
 package com.chanapps.four.test;
 
 import android.app.Activity;
-import android.app.WallpaperManager;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.*;
 import android.webkit.WebView;
-import android.widget.Toast;
+import android.widget.*;
 import com.chanapps.four.data.Captcha;
-import com.chanapps.four.data.ChanDatabaseHelper;
 import com.chanapps.four.data.Recaptcha;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 
+import javax.security.auth.login.LoginException;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 
 public class PostReplyActivity extends Activity {
 
@@ -42,7 +34,23 @@ public class PostReplyActivity extends Activity {
     public static final String RECAPTCHA_NOSCRIPT_URL = "http://www.google.com/recaptcha/api/noscript?k=";
     public static final String RECAPTCHA_PUBLIC_KEY = "6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc";
     public static final String RECAPTCHA_URL = RECAPTCHA_NOSCRIPT_URL + RECAPTCHA_PUBLIC_KEY;
+    public static final String CAPTCHA_DEFAULT_URL = "file:///android_res/drawable/captcha.png";
 
+    private static final int IMAGE_CAPTURE = 0;
+    private static final int IMAGE_GALLERY = 1;
+
+    private ImageButton cameraButton;
+    private ImageButton pictureButton;
+    private ImageButton rotateLeftButton;
+    private ImageButton rotateRightButton;
+
+    WebView recaptchaView;
+
+    private ImageView imagePreview;
+    private int angle = 0;
+
+    private String fileName;
+    private Uri imageUri;
     String boardCode = null;
     int threadNo = 0;
 
@@ -79,13 +87,195 @@ public class PostReplyActivity extends Activity {
         }
 
         setContentView(R.layout.post_reply_activity_layout);
+
+        imagePreview = (ImageView)findViewById(R.id.post_reply_image_preview);
+
+        cameraButton = (ImageButton)findViewById(R.id.post_reply_camera_button);
+        pictureButton = (ImageButton)findViewById(R.id.post_reply_picture_button);
+        rotateLeftButton = (ImageButton)findViewById(R.id.post_reply_rotate_left_button);
+        rotateRightButton = (ImageButton)findViewById(R.id.post_reply_rotate_right_button);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startCamera();
+            }
+        });
+        pictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGallery();
+            }
+        });
+        rotateLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rotateLeft();
+            }
+        });
+        rotateRightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rotateRight();
+            }
+        });
+
+        recaptchaView = (WebView) findViewById(R.id.post_reply_recaptcha_webview);
+        recaptchaView.getSettings().setAllowFileAccess(true);
         LoadCaptchaTask loadCaptchaTask = new LoadCaptchaTask(getApplicationContext());
         loadCaptchaTask.execute(RECAPTCHA_URL);
     }
 
-    private void loadCaptcha(Captcha captcha) {
-        WebView recaptchaView = (WebView) findViewById(R.id.post_reply_recaptcha_webview);
-        recaptchaView.loadUrl(captcha.ImageUrl);
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String msg = "Couldn't load activity image";
+        try {
+        if (requestCode == IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK && imageUri != null) {
+                msg = "Added image to post";
+                setImagePreview();
+            }
+            else {
+                Log.e(TAG, "Couldn't load camera image");
+            }
+        }
+        else if (requestCode == IMAGE_GALLERY) {
+            if (resultCode == RESULT_OK) {
+                msg = "Added image to post";
+                imageUri = data.getData();
+                setImagePreview();
+            }
+            else {
+                Log.e(TAG, "Couldn't load gallery image");
+            }
+        }
+        else {
+            msg = "Unknown activity";
+        }
+        }
+        catch (Exception e) {
+            Log.e(TAG, msg, e);
+        }
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private Bitmap getImagePreviewBitmap() throws Exception {
+        return getImagePreviewBitmap(true);
+    }
+
+    private Bitmap getImagePreviewBitmap(boolean useWidth) throws Exception {
+        if (imageUri == null) {
+            throw new Exception("Null image URI for preview");
+        }
+
+        InputStream in = getContentResolver().openInputStream(imageUri);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+
+        Bitmap previewBitmap = BitmapFactory.decodeStream(in, null, options);
+        int requiredWidth = imagePreview.getWidth();
+        int scale = 1;
+        if (useWidth) {
+            while (options.outWidth / scale / 2>= requiredWidth) {
+                scale *= 2;
+            }
+        }
+        else {
+            while (options.outHeight / scale / 2>= requiredWidth) {
+                scale *= 2;
+            }
+        }
+
+        InputStream inScale = getContentResolver().openInputStream(imageUri);
+        BitmapFactory.Options scaleOptions = new BitmapFactory.Options();
+        scaleOptions.inSampleSize = scale;
+        return BitmapFactory.decodeStream(inScale, null, scaleOptions);
+    }
+
+    private void resetImagePreview() {
+        rotateLeftButton.setVisibility(View.VISIBLE);
+        rotateRightButton.setVisibility(View.VISIBLE);
+        imagePreview.setVisibility(View.VISIBLE);
+        imagePreview.setPadding(0, 0, 0, 16);
+        angle = 0;
+    }
+
+    private void setImagePreview() {
+        try {
+            Bitmap b = getImagePreviewBitmap();
+            if (b != null) {
+                resetImagePreview();
+                imagePreview.setImageBitmap(b);
+                Log.e(TAG, "Image: " + imageUri.toString() + " dimensions: " + b.getWidth() + "x" + b.getHeight());
+            }
+            else {
+                Log.e(TAG, "Image: " + imageUri+toString() + " null bitmap");
+            }
+        }
+        catch (Exception e) {
+            Log.e(TAG, "Couldn't load image bitmap", e);
+        }
+    }
+
+    private void rotateLeft() {
+        rotateImagePreview(-90);
+    }
+
+    private void rotateRight() {
+        rotateImagePreview(90);
+    }
+
+    private void rotateImagePreview(int theta) {
+        Log.e(TAG, "rotate right...");
+        try {
+            Bitmap b = getImagePreviewBitmap(false);
+            if (b != null) {
+                Log.e(TAG, "ready to rotate right...");
+                Matrix matrix = new Matrix();
+                angle += theta;
+                matrix.postRotate(angle);
+                Bitmap rotatedBitmap = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(), matrix, true);
+                imagePreview.setImageBitmap(rotatedBitmap);
+            }
+            else {
+                Log.e(TAG, "couldn't get image bitmap for rotation");
+            }
+        }
+        catch (Exception e) {
+            Log.e(TAG, "Couldn't rotate image bitmap", e);
+        }
+    }
+
+    private void startCamera() {
+        Log.d(TAG, "starting camera...");
+        fileName = java.util.UUID.randomUUID().toString() + ".jpg";
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, fileName);
+        values.put(MediaStore.Images.Media.DESCRIPTION,
+                "Image capture by camera");
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        if (imageUri != null) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, IMAGE_CAPTURE);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Unable to load camera, try again", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void startGallery() {
+        Log.d(TAG, "starting gallery...");
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, IMAGE_GALLERY);
     }
 
     @Override
@@ -133,35 +323,65 @@ public class PostReplyActivity extends Activity {
             this.context = context;
         }
 
+        @Override
         protected HttpResponse doInBackground(String... params) {
-             try {
+            AndroidHttpClient client = AndroidHttpClient.newInstance("Android");
+            try {
                  String url = params[0];
                  HttpGet request = new HttpGet(url);
-                 AndroidHttpClient client = AndroidHttpClient.newInstance("Android");
                  return client.execute(request);
-             } catch (Exception e) {
+            } catch (Exception e) {
+                String msg = "Couldn't get recaptcha";
                 Log.e(TAG, "Error getting recaptcha url", e);
-             }
-             return null;
-         }
+            }
+            finally {
+                if (client != null) {
+                    client.close();
+                }
+            }
+            return null;
+        }
 
-         protected void onPostExecute(HttpResponse response) {
-             if (response == null) {
-                 return;
-             }
-             try {
-                 BufferedReader r = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-                 StringBuilder s = new StringBuilder();
-                 String line;
-                 while ((line = r.readLine()) != null) {
-                     s.append(line);
-                 }
-                 Captcha c = Recaptcha.GetCaptcha(s.toString());
-                 loadCaptcha(c);
-             } catch (Exception e) {
+        @Override
+        protected void onCancelled() {
+            String msg = "Couldn't load captcha";
+            Log.e(TAG, msg);
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            recaptchaView.loadUrl(CAPTCHA_DEFAULT_URL);
+        }
+
+        @Override
+        protected void onPostExecute(HttpResponse response) {
+            String msg = "Couldn't load captcha";
+            if (response == null) {
+                Log.e(TAG, "Null response loading recaptcha");
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                return;
+            }
+            try {
+                BufferedReader r = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                StringBuilder s = new StringBuilder();
+                String line;
+                while ((line = r.readLine()) != null) {
+                    s.append(line);
+                }
+                Captcha c = Recaptcha.GetCaptcha(s.toString());
+                String url = c.ImageUrl;
+                if (url != null && !url.equalsIgnoreCase("false")) {
+                    Log.e(TAG, "Found recaptcha url: " + url);
+                    recaptchaView.loadUrl(c.ImageUrl);
+                }
+                else {
+                    Log.e(TAG, "Error reading recaptcha response");
+                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    recaptchaView.loadUrl(CAPTCHA_DEFAULT_URL);
+                }
+            } catch (Exception e) {
                 Log.e(TAG, "Error reading recaptcha response", e);
-             }
-         }
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                recaptchaView.loadUrl(CAPTCHA_DEFAULT_URL);
+            }
+        }
     }
 
 }
