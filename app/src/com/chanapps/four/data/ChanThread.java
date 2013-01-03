8af2dc13b6048d88d5650fbf70a086ecb19f50a1
John Arley Burns <johnarleyburns@gmail.com>
2013-01-03 15:24:26 +0100

    fixed #8 next prev buttons on full screen image view

--- app/src/com/chanapps/four/data/ChanThread.java
+++ app/src/com/chanapps/four/data/ChanThread.java
@@ -26,4 +26,28 @@ public class ChanThread extends ChanPost {
         this.posts = mergedPosts.toArray(new ChanPost[0]);
     }
 
+    public ChanPost[] getPrevNextPosts(long postNo) {
+        ChanPost prevPost = null;
+        ChanPost nextPost = null;
+        if (posts != null) {
+            for (int i = 0; i < posts.length; i++) {
+                ChanPost post = posts[i];
+                if (post.no == postNo) {
+                    for (int j = i - 1; j >= 0; j--)
+                        if (posts[j].w > 0) {
+                            prevPost = posts[j];
+                            break;
+                        }
+                    for (int j = i + 1; j < posts.length; j++)
+                        if (posts[j].w > 0) {
+                            nextPost = posts[j];
+                            break;
+                        }
+                    break;
+                }
+            }
+        }
+        ChanPost[] posts = { prevPost, nextPost };
+        return posts;
+    }
 }
