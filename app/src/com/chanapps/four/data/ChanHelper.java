0b13ca53c19b9f043891230e20792943a32caac4
John Arley Burns <johnarleyburns@gmail.com>
2013-01-28 10:31:11 +0100

    fixed #94 better popup menus

--- app/src/com/chanapps/four/data/ChanHelper.java
+++ app/src/com/chanapps/four/data/ChanHelper.java
@@ -28,7 +28,7 @@ public class ChanHelper {
     public static final String POST_NO = "postNo";
     public static final String TIM = "tim";
     public static final String TEXT = "text";
-    public static final String QUOTE_TEXT = "quoteText";
+    //public static final String QUOTE_TEXT = "quoteText";
     public static final String NAME = "name";
     public static final String EMAIL = "email";
     public static final String SUBJECT = "subject";
