4c43b083c05041f020a1cfcfb9354b0bcd108d3e
John Arley Burns <johnarleyburns@gmail.com>
2013-02-19 17:54:11 +0100

    partial #29 - authorization button for authing pass

--- app/src/com/chanapps/four/fragment/PassSettingsFragment.java
+++ app/src/com/chanapps/four/fragment/PassSettingsFragment.java
@@ -2,10 +2,15 @@ package com.chanapps.four.fragment;
 
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
 import com.chanapps.four.activity.R;
+import com.chanapps.four.activity.SettingsActivity;
+import com.chanapps.four.service.NetworkProfileManager;
 
 
 /**
@@ -15,13 +20,20 @@ import com.chanapps.four.activity.R;
  * Time: 3:12 PM
  * To change this template use File | Settings | File Templates.
  */
-public class PassSettingsFragment extends PreferenceFragment
+public class PassSettingsFragment
+    extends PreferenceFragment
+    implements Preference.OnPreferenceChangeListener
 {
 
     public static String PREF_PASS_CLOSE_BUTTON = "pref_pass_close_button";
+    public static String PREF_PASS_AUTH_BUTTON = "pref_pass_auth_button";
     public static String TAG = PassSettingsFragment.class.getSimpleName();
 
     protected DialogInterface.OnDismissListener dismissListener;
+    protected SharedPreferences prefs;
+    protected Preference passEnabled;
+    protected Preference authButton;
+    protected Preference closeButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,10 +41,25 @@ public class PassSettingsFragment extends PreferenceFragment
 
         addPreferencesFromResource(R.xml.pass_preferences);
 
-        Preference closeButton = findPreference(PREF_PASS_CLOSE_BUTTON);
+        passEnabled = findPreference(SettingsActivity.PREF_PASS_ENABLED);
+        passEnabled.setOnPreferenceChangeListener(this);
+
+        authButton = findPreference(PREF_PASS_AUTH_BUTTON);
+        authButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(getActivity(), "Authorized", Toast.LENGTH_SHORT).show();
+                PassSettingsFragment.this.dismiss();
+                return true;
+            }
+        });
+        updateAuthorizeVisibility();
+
+        closeButton = findPreference(PREF_PASS_CLOSE_BUTTON);
         closeButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
+                ensurePrefs().edit().putBoolean(SettingsActivity.PREF_PASS_ENABLED, false).commit();
                 PassSettingsFragment.this.dismiss();
                 return true;
             }
@@ -54,6 +81,45 @@ public class PassSettingsFragment extends PreferenceFragment
         ft.commit();
         if (dismissListener != null)
             dismissListener.onDismiss(null);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference pref, Object newValue) {
+        if (newValue instanceof Boolean && ((Boolean) newValue).booleanValue())
+            updateAuthorizeVisibility(true);
+        return true;
+    }
+
+    public SharedPreferences ensurePrefs() {
+        if (prefs == null)
+            prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        return prefs;
+    }
+
+    private boolean isPassEnabled() {
+        return ensurePrefs().getBoolean(SettingsActivity.PREF_PASS_ENABLED, false);
+    }
+
+    private boolean isPassAvailable() {
+        switch (NetworkProfileManager.instance().getCurrentProfile().getConnectionType()) {
+            case WIFI:
+                return true;
+            case MOBILE:
+            case NO_CONNECTION:
+            default:
+                return false;
+        }
+    }
+
+    private void updateAuthorizeVisibility() {
+        updateAuthorizeVisibility(isPassEnabled());
+    }
+
+    private void updateAuthorizeVisibility(boolean passEnabled) {
+        if (isPassAvailable() && passEnabled)
+            authButton.setEnabled(true);
+        else
+            authButton.setEnabled(false);
     }
 
 }
