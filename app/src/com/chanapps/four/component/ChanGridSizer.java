bcd51fcd4f19b96ca9e5ff08a810b5670a7facf0
John Arley Burns <johnarleyburns@gmail.com>
2013-02-26 13:32:55 +0100

    partial #186 proper sizing with padding

--- app/src/com/chanapps/four/component/ChanGridSizer.java
+++ app/src/com/chanapps/four/component/ChanGridSizer.java
@@ -40,11 +40,21 @@ public class ChanGridSizer {
     private int columnWidth = 0;
     private int columnHeight = 0;
     private int maxColumnWidth = 200;
+    private int paddingTop = 0;
+    private int paddingLeft = 0;
+    private int paddingRight = 0;
+    private int paddingBottom = 0;
 
     public ChanGridSizer(View g, Display d, ServiceType serviceType) {
     	if (g instanceof GridView) {
     		this.g = (GridView)g;
     	}
+        else if (g != null) { // we support padding only on container views, not on the grid view itself
+            paddingTop = g.getPaddingTop();
+            paddingLeft = g.getPaddingLeft();
+            paddingRight = g.getPaddingRight();
+            paddingBottom = g.getPaddingBottom();
+        }
         this.d = d;
         int layoutMask = g.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
         int[] columnWidthArray =
@@ -74,10 +84,10 @@ public class ChanGridSizer {
     public void sizeGridToDisplay() {
         Point size = new Point();
         d.getSize(size);
-        int width = size.x;
-        int height = size.y;
+        int width = size.x - paddingLeft - paddingRight;
+        int height = size.y - paddingTop - paddingBottom;
         numColumns = width / maxColumnWidth == 1 ? 2 : width / maxColumnWidth;
-        columnWidth = (width - 15) / numColumns;
+        columnWidth = width / numColumns;
         columnHeight = columnWidth;
 		if (DEBUG) Log.i(TAG, "sizeGridToDisplay width: " + width + ", height: " + height + ", numCols: " + numColumns);
         if (g != null) {
