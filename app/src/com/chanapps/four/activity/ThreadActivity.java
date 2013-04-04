7800987b8374805f846fbc4ba1c1ad9ff83fa57f
Grzegorz Nittner <grzegorz.nittner@vodafone.com>
2013-04-04 19:43:32 +0200

    UniversalImageLoader can store images in our cache folder

--- app/src/com/chanapps/four/activity/ThreadActivity.java
+++ app/src/com/chanapps/four/activity/ThreadActivity.java
@@ -480,6 +480,7 @@ public class ThreadActivity
         int postW = 0;
         int postH = 0;
         int listPosition = 0;
+        String fullImageLocation = null;
 
         public ExpandImageOnClickListener(final Cursor cursor, final int expandable, final View itemView) {
             this.expandable = expandable;
@@ -499,6 +500,8 @@ public class ThreadActivity
             long postTim = cursor.getLong(cursor.getColumnIndex(ChanHelper.POST_TIM));
             String postExt = cursor.getString(cursor.getColumnIndex(ChanHelper.POST_EXT));
             postImageUrl = postTim > 0 ? ChanPost.imageUrl(boardCode, postTim, postExt) : null;
+            long postId = cursor.getLong(cursor.getColumnIndex(ChanHelper.POST_ID));
+            fullImageLocation = ChanFileStorage.getBoardCacheDirectory(getBaseContext(), boardCode) + "/" + postId + postExt;
         }
 
         @Override
@@ -605,6 +608,7 @@ public class ThreadActivity
                     .imageScaleType(ImageScaleType.EXACT)
                     .cacheOnDisc()
                     .imageSize(imageSize)
+                    .fullSizeImageLocation(fullImageLocation)
                     .resetViewBeforeLoading()
                     .build();
 
