aeef89c289ca2efbc8bdaee69a5408ad190bda15
John Arley Burns <johnarleyburns@gmail.com>
2013-09-13 12:21:36 +0200

    enhanced exception reporting for google analytics

--- app/src/com/chanapps/four/FourChannerApp.java
+++ app/src/com/chanapps/four/FourChannerApp.java
@@ -2,11 +2,14 @@ package com.chanapps.four;
 
 import com.android.gallery3d.app.GalleryAppImpl;
 import com.android.gallery3d.data.DataManager;
+import com.chanapps.four.component.AnalyticsExceptionParser;
 import com.chanapps.four.component.BillingComponent;
 import com.chanapps.four.data.ChanFileStorage;
 import com.chanapps.four.data.UserStatistics;
 import com.chanapps.four.gallery.ChanOffLineSource;
 import com.chanapps.four.gallery.ChanSource;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.ExceptionReporter;
 
 /**
  * @author "Grzegorz Nittner" <grzegorz.nittner@gmail.com>
@@ -26,6 +29,19 @@ public class FourChannerApp extends GalleryAppImpl {
     @Override
     public void onCreate() {
         super.onCreate();
+        setupEnhancedExceptions();
         BillingComponent.getInstance(getApplicationContext()).checkForNewPurchases();
     }
+
+    protected void setupEnhancedExceptions() {
+        EasyTracker.getInstance().setContext(this);
+        // Change uncaught exception parser...
+        // Note: Checking uncaughtExceptionHandler type can be useful if clearing ga_trackingId during development to disable analytics - avoid NullPointerException.
+        Thread.UncaughtExceptionHandler uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+        if (uncaughtExceptionHandler instanceof ExceptionReporter) {
+            ExceptionReporter exceptionReporter = (ExceptionReporter) uncaughtExceptionHandler;
+            exceptionReporter.setExceptionParser(new AnalyticsExceptionParser());
+        }
+    }
+
 }
