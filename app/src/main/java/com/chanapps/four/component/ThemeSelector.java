db4fa5b609d0248d0da4c1d96ff8bde5fa134394
Alessandro Digilio <alessandro.digilio@hotmail.it>
2020-04-22 15:13:40 +0200

    general cleanup:
    -cleanup manifest
    -cleanup unused languages
    -cleanup comments in themes xml

--- app/src/main/java/com/chanapps/four/component/ThemeSelector.java
+++ app/src/main/java/com/chanapps/four/component/ThemeSelector.java
@@ -27,18 +27,13 @@ public class ThemeSelector {
     protected static final String TAG = ThemeSelector.class.getSimpleName();
     protected static final boolean DEBUG = false;
     protected static final int LIGHT_THEME = R.style.AppTheme;
-    public static final int DEFAULT_THEME = LIGHT_THEME;
-    //protected static final float LIGHT_LUX_THRESHOLD = 5;
-    //protected static final float LUX_MIN_DELTA = 10;
     protected static final int DARK_THEME = R.style.AppTheme_Dark;
+    public static final int DEFAULT_THEME = LIGHT_THEME;
     protected static ThemeSelector themeSelector;
 
     protected String themeType;
     protected int themeId = LIGHT_THEME;
     protected Context context;
-    //protected SensorManager sensorManager;
-    //protected Sensor lightSensor;
-    //protected float lux = 100; // default to room lighting
     protected SharedPreferences.OnSharedPreferenceChangeListener themeChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
@@ -52,34 +47,6 @@ public class ThemeSelector {
         }
     };
 
-    /*
-    protected SensorEventListener lightSensorListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            if (!context.getString(R.string.pref_theme_auto_value).equals(themeType))
-                return;
-            if (event == null
-                    || event.sensor == null
-                    || event.sensor.getType() != Sensor.TYPE_LIGHT
-                    || event.values == null
-                    || event.values.length < 1
-                    )
-                return;
-            float currentLux = event.values[0];
-            if (Math.abs(lux - currentLux) >= LUX_MIN_DELTA) {
-                lux = currentLux;
-                int newTheme = ambientLuxTheme();
-                if (themeId != newTheme)
-                    updateTheme(newTheme);
-            }
-        }
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            ; // ignore
-        }
-    };
-    */
-
     protected ThemeSelector(Context context) {
         this.context = context;
 
@@ -90,10 +57,6 @@ public class ThemeSelector {
             Log.i(TAG, "ThemeSelector() set to " + (themeId == DARK_THEME ? "dark" : "light") + " theme");
         prefs.registerOnSharedPreferenceChangeListener(themeChangeListener);
 
-        //sensorManager = (SensorManager)context.getSystemService(Service.SENSOR_SERVICE);
-        //lightSensor = sensorManager == null ? null : sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        //if (lightSensor != null)
-        //    sensorManager.registerListener(lightSensorListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     public static ThemeSelector instance(Context context) {
@@ -114,8 +77,6 @@ public class ThemeSelector {
         if (context.getString(R.string.pref_theme_light_value).equals(themeType)) id = LIGHT_THEME;
         else if (context.getString(R.string.pref_theme_dark_value).equals(themeType))
             id = DARK_THEME;
-            //else if (context.getString(R.string.pref_theme_auto_value).equals(themeType))
-            //    id = ambientLuxTheme(); // should be fixed to check sensor lux levels
         else id = DEFAULT_THEME;
         if (DEBUG) Log.i(TAG, "calcThemeId('" + themeType + "') = " + id);
         return id;
@@ -124,15 +85,6 @@ public class ThemeSelector {
     public boolean isDark() {
         return themeId == R.style.AppTheme_Dark;
     }
-
-    /*
-    protected int ambientLuxTheme() {
-        if (lux >= LIGHT_LUX_THRESHOLD)
-            return LIGHT_THEME;
-        else
-            return DARK_THEME;
-    }
-    */
 
     public interface ThemeActivity {
         int getThemeId();
